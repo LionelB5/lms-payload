6f42e04356ef943091deeb329d29099455d5461f
Lionel Bersée <lionel.h.bersee@gmail.com>
2026-02-20 00:41:58 +1100

    setup access control on courses

--- src/collections/courses/Courses.ts
+++ src/collections/courses/Courses.ts
@@ -5,6 +5,12 @@ import { QuizBlock } from '../blocks/QuizBlock'
 export const Courses: CollectionConfig = {
   slug: 'courses',
   admin: { useAsTitle: 'title' },
+  access: {
+    create: ({ req: { user } }) => user?.collection === 'users',
+    read: ({ req: { user } }) => Boolean(user),
+    update: ({ req: { user } }) => user?.collection === 'users',
+    delete: ({ req: { user } }) => user?.collection === 'users',
+  },
   fields: [
     { name: 'title', label: 'Title', type: 'text', required: true },
     { name: 'description', label: 'Description', type: 'textarea', required: true },
